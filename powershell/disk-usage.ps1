2afe12bd7b22ae3c3bebee907bd009efd7a4e4f7
Aleesha Najeeb <50927666+200394602@users.noreply.github.com>
2019-07-30 04:17:51 -0400

    LAB 3

---
+++ powershell/disk-usage.ps1
@@ -0,0 +1,20 @@
+﻿#Create a script which produces a report showing the space usage for the filesystems in the computer.
+
+#Use the get-wmiobject command as shown below to get a collection of disk filesystem objects.
+#Only include filesystems which have a non-zero size. Use a pipeline with a where-object filter.
+#Your report must be a table showing only the filesystem drive letter, size of filesystem, free space, and providername.
+#Use format-table to format your output and your numbers must be human-friendly.
+
+function Show-SpaceUsage {
+  param (
+    [long]$MinFreeInBytes 
+  )
+  $drives = Get-WmiObject -Class win32_logicaldisk
+  $filesystems = $drives | Where-Object size -GT 0
+  $filesystems | Format-Table -AutoSize DeviceID,
+                                    @{ n= "Size(GB)" ; e= {$_.size / 1gb -as [int]} } ,
+                                    @{ n= "Free(GB)" ; e= {$_.freespace / 1gb -as [int]} } , 
+                                    ProviderName
+}
+
+Show-SpaceUsage
---
+++ powershell/software-report.ps1
@@ -0,0 +1,3 @@
+﻿gwmi -class win32_product -filter "vendor!='Microsoft Corporation'" | 
+sort installdate | 
+format-table Name, Vendor, Version, InstallDate 
